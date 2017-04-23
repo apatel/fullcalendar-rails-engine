b3e04a0c44117f7f465b2c7153bcf5821869b967
Anita Patel <anita.patel@tripcraft.com>
2017-04-23 18:31:22 -0400

    testing

--- app/assets/javascripts/fullcalendar_engine/fullcalendar.js
+++ app/assets/javascripts/fullcalendar_engine/fullcalendar.js
@@ -120,7 +120,8 @@ var fcViews = fc.views = {};
 
 
 $.fn.fullCalendar = function(options) {
-
+  console.log "OPTIONS"
+  console.log options
 
 	// method calling
 	if (typeof options == 'string') {
